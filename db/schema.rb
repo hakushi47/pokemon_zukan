9564684687f83501e1331465f585baa150f8a49f
hareame1947 <hareame1947@gmail.com>
2023-06-07 15:44:15 +0900

    モデル作成、seed作成　css反映確認済

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_061616) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_063748) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_061616) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "pokemons", force: :cascade do |t|
+    t.string "name"
+    t.integer "number"
+    t.string "zokusei"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,71 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+pokemon = Pokemon.create!(name: "フシギダネ",
+						 zokusei: "くさ",
+						 number: 1,
+						 description: "うまれてから しばらくの あいだは せなかの タネから えいようを もらって おおきく そだつ。")
+pokemon.image.attach(io: File.open(Rails.root.join('app/assets/images/hushigidane.png')),
+                  filename: 'hushigidane.png')
+
+pokemon = Pokemon.create!(name: "フシギソウ",
+						 zokusei: "くさ",
+						 number: 2,
+						 description: "せなかの つぼみが ふくらみだすと あまい においが ただよいはじめる。たいりんの はなが さく まえぶれ。")
+pokemon.image.attach(io: File.open(Rails.root.join('app/assets/images/hushigisou.png')),
+                  filename: 'hushigisou.png')
+
+pokemon = Pokemon.create!(name: "フシギバナ",
+						 zokusei: "くさ",
+						 number: 3,
+						 description: "あめの ふった よくじつは せなかの はなの かおりが つよまる。かおりに さそわれ ポケモンが あつまる。")
+pokemon.image.attach(io: File.open(Rails.root.join('app/assets/images/hushigibana.png')),
+                  filename: 'hushigibana.png')
+
+
+pokemon = Pokemon.create!(name: "ヒトカゲ",
+						 zokusei: "ほのお",
+						 number: 4,
+						 description: "ヒトカゲの　ほのおの　しっぽは　いのちの　ともしび。　げんきなときは　ほのおも　ちからづよく　もえあがる。")
+pokemon.image.attach(io: File.open(Rails.root.join('app/assets/images/hitokage.png')), filename:'hitokage.png')
+
+pokemon = Pokemon.create!(name: "リザード",
+						 zokusei: "ほのお",
+						 number: 5,
+						 description: "リザードが くらす いわやまを よなかに みあげると しっぽの ほのおが ほしのように みえる。")
+pokemon.image.attach(io: File.open(Rails.root.join('app/assets/images/riza-do.png')), filename:'riza-do.png')
+
+pokemon = Pokemon.create!(name: "リザードン",
+						 zokusei: "ほのお",
+						 number: 6,
+						 description: "くるしい たたかいを けいけんした リザードンほど ほのおの おんどが たかくなると いわれている。
+")
+pokemon.image.attach(io: File.open(Rails.root.join('app/assets/images/riza-don.png')), filename:'riza-don.png')
+
+
+
+pokemon = Pokemon.create!(name: "ゼニガメ",
+						 zokusei: "みず",
+						 number: 7,
+						 description: "こうらに とじこもり みを まもる。 あいての すきを みのがさず みずを ふきだして はんげきする。")
+pokemon.image.attach(io: File.open(Rails.root.join('app/assets/images/zenigame.png')),
+                  filename: 'zenigame.png')
+
+pokemon = Pokemon.create!(name: "カメール",
+						 zokusei: "みず",
+						 number: 8,
+						 description: "いちまんねんの じゅみょうを もつと いわれている。ふさふさの しっぽは ながいきの シンボルとして にんき。")
+pokemon.image.attach(io: File.open(Rails.root.join('app/assets/images/kame-ru.png')),
+                  filename: 'kame-ru.png')
+
+pokemon = Pokemon.create!(name: "カメックス",
+						 zokusei: "みず",
+						 number: 9,
+						 description: "こうらの ふんしゃこうの ねらいは せいかく。みずの だんがんを 50メートル はなれた あきかんに めいちゅうさせる ことが できるぞ。")
+pokemon.image.attach(io: File.open(Rails.root.join('app/assets/images/kamekkusu.png')),
+                  filename: 'kamekkusu.png')
+
+
+pokemon = Pokemon.create!(name: "イーブイ",
+						 zokusei: "ノーマル",
+						 number: 133,
+						 description: "しんかのとき すがたと のうりょくが かわることで きびしい かんきょうに たいおうする めずらしい ポケモン。")
+pokemon.image.attach(io: File.open(Rails.root.join('app/assets/images/i-bui.png')),
+                  filename: 'i-bui.png')
