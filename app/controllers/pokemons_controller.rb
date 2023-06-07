e1bc61f88857b09f482734ede11fde2cf0b5528d
hareame1947 <hareame1947@gmail.com>
2023-06-07 15:36:47 +0900

    detailアクション作成まで

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,4 +1,8 @@
 class PokemonsController < ApplicationController
     def index
     end
+
+    def show
+      @pokemon = Pokemon.find(1)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get 'index' => 'pokemons#index'
+  get 'index'  => 'pokemons#index'
+  get 'detail' => 'pokemons#show'
 end
