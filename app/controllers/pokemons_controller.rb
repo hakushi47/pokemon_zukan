edb12639e2423842270e9ca54189affa6931a574
hareame1947 <hareame1947@gmail.com>
2023-06-07 15:53:22 +0900

    ポケモン登録機能を追加

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -5,4 +5,21 @@ class PokemonsController < ApplicationController
     def show
       @pokemon = Pokemon.find(params[:id])
     end
+
+    def new
+      @pokemon = Pokemon.new
+    end
+
+    
+    def create
+      @pokemon = Pokemon.new
+      @pokemon.name = params[:pokemon][:name]
+      @pokemon.zokusei = params[:pokemon][:zokusei]
+      @pokemon.number = params[:pokemon][:number] 
+      @pokemon.description  = params[:pokemon][:description]
+      @pokemon.image = params[:pokemon][:image]
+      @pokemon.save
+      redirect_to @pokemon
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'index'  => 'pokemons#index'
+  get 'index'      => 'pokemons#index'
   get 'detail/:id' => 'pokemons#show'
+  get 'new'        => 'pokemons#new'
+  post 'new'       => 'pokemons#create'
 end
